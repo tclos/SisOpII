b92eae2f04e2548c37155223acb4eb412fb25f68
biasoviero <biasoviero@gmail.com>
2025-10-17 17:54:29 -0300

    Update transactionServer.cpp

--- T1/transactions/transactionServer.cpp
+++ T1/transactions/transactionServer.cpp
@@ -13,15 +13,17 @@ void handleTransactionRequest(const Packet& request_packet, const struct sockadd
 
         std::tuple<TransactionStatus, float, int> result = server_data.processTransaction(source_ip, request_packet.data.req.dest_addr, value, seqn);
 
-        TransactionStatus status = std::get<0>(result);;
-        float balance = std::get<1>(result);;
+        TransactionStatus status = std::get<0>(result);
+        float balance = std::get<1>(result);
+        int last_req = std::get<2>(result);
 
         Packet response_packet;
         response_packet.type = htons(TRANSACTION_ACK);
         response_packet.data.ack.seqn = htonl(seqn);
-
         response_packet.data.ack.status = htonl(static_cast<uint32_t>(status));
         response_packet.data.ack.new_balance = balance;
+
+        response_packet.data.ack.last_req = htonl(last_req);
         
         server_socket.sendPacket(response_packet, client_addr);
 
